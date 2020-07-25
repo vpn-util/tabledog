51f1cb7155884d82e68772177d17f76efa07277a
Luis Felger <firefligher.tornado@gmail.com>
2020-07-25 10:14:51 +0200

    Improved the Ctrl+C handler on Windows

--- src/App.php
+++ src/App.php
@@ -25,10 +25,22 @@ class App {
         #
         # NOTE: The pcntl extension is only available on unixoid systems.
 
+        $windows = FALSE;
+
         if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
             \pcntl_signal(SIGINT, function (int $signo) {
                 App::$RUNNING = FALSE;
             }, FALSE);
+        } else {
+            sapi_windows_set_ctrl_handler(function (int $event) {
+                if ($event !== PHP_WINDOWS_EVENT_CTRL_C) {
+                    return;
+                }
+
+                App::$RUNNING = FALSE;
+            });
+
+            $windows = TRUE;
         }
 
         # Launching the server
@@ -39,7 +51,17 @@ class App {
         $dirty = FALSE;
 
         while (App::$RUNNING) {
-            $connections = $server->proceed($dirty);
+            # If we're on Windows, we always enable the dirty flag:
+            #  - The dirty flag sets a defined timeout for the
+            #    socket_select-operation.
+            #  - On Windows, Ctrl+C will not cause a SIGINT and the registered
+            #    Ctrl+C handler is prevented from execution until the
+            #    socket_select-operation returns.
+            #  => We need to ensure that socket_select returns periodically on
+            #     Windows, otherwise terminating the process will depend on the
+            #     return of socket_select.
+
+            $connections = $server->proceed($dirty || $windows);
 
             foreach ($connections as $cnt) {
                 $cnt->setBufferedOutput($cnt->getBufferedInput());
